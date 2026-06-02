3ce8d818f422c8d99fd687089faecdaf0231965d
hakizao6074 <davizinhodjar@gmail.com>
2026-06-02 10:31:45 -0300

    fit: screens de perfil e conta

--- src/screens/Contascreen.js
+++ src/screens/Contascreen.js
@@ -1,9 +1,264 @@
-import { View, Text } from 'react-native'
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function Contas() {
+export default function Perfil() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Contas</Text>
+    <View style={styles.container}>
+      <Text style={styles.tittle}>Conta</Text>
+
+      <View style={styles.containerAccnt}>
+        
+        <View style={styles.PerfilLine}>
+          
+          <View style={styles.Perfil}>
+            <Ionicons name="person" size={77} color="#c1bfbf85" />
+          </View>
+
+          <View style={styles.areaName}>
+            <Text style={styles.TextPerfil}>Seu Nome</Text>
+
+            <Text style={styles.TextID}>@SeuNome</Text>
+
+            <Text style={styles.Bio}>
+              Gosto de filmes animados
+            </Text>
+          </View>
+
+        </View>
+
+        <View style={styles.optionAccount}>
+
+          <Pressable style={styles.buttonOption}>
+            <View style={styles.leftOption}>
+              <Ionicons
+                name="mail-outline"
+                size={22}
+                color="#444"
+              />
+
+              <Text style={styles.textOption}>
+                E-mail
+              </Text>
+            </View>
+
+            <Ionicons
+              name="chevron-forward"
+              size={20}
+              color="#999"
+            />
+          </Pressable>
+
+          <Pressable style={styles.buttonOption}>
+            <View style={styles.leftOption}>
+              <Ionicons
+                name="lock-closed-outline"
+                size={22}
+                color="#444"
+              />
+
+              <Text style={styles.textOption}>
+                Senha
+              </Text>
+            </View>
+
+            <Ionicons
+              name="chevron-forward"
+              size={20}
+              color="#999"
+            />
+          </Pressable>
+
+          <Pressable style={styles.buttonOption}>
+            <View style={styles.leftOption}>
+              <Ionicons
+                name="notifications-outline"
+                size={22}
+                color="#444"
+              />
+
+              <Text style={styles.textOption}>
+                Notificações
+              </Text>
+            </View>
+
+            <Ionicons
+              name="chevron-forward"
+              size={20}
+              color="#999"
+            />
+          </Pressable>
+
+          <Pressable style={styles.buttonOption}>
+            <View style={styles.leftOption}>
+              <Ionicons
+                name="shield-outline"
+                size={22}
+                color="#444"
+              />
+
+              <Text style={styles.textOption}>
+                Privacidade
+              </Text>
+            </View>
+
+            <Ionicons
+              name="chevron-forward"
+              size={20}
+              color="#999"
+            />
+          </Pressable>
+
+          <Pressable style={styles.buttonOption}>
+            <View style={styles.leftOption}>
+              <Ionicons
+                name="moon-outline"
+                size={22}
+                color="#444"
+              />
+
+              <Text style={styles.textOption}>
+                Tema
+              </Text>
+            </View>
+
+            <Ionicons
+              name="chevron-forward"
+              size={20}
+              color="#999"
+            />
+          </Pressable>
+
+        </View>
+
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffffff",
+    flex: 1,
+  },
+
+  tittle: {
+    fontSize: 35,
+    marginLeft: 20,
+    marginTop: 10,
+    fontWeight: "600",
+  },
+
+  containerAccnt: {
+    flex: 1,
+    alignItems: "center",
+  },
+
+  PerfilLine: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+
+    height: 130,
+    width: 360,
+
+    backgroundColor: "#ffffff",
+
+    borderWidth: 1,
+    borderColor: "#eaeaeaa3",
+    borderRadius: 10,
+
+    marginTop: 10,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+
+    shadowOpacity: 0.15,
+    shadowRadius: 4,
+
+    elevation: 5,
+  },
+
+  Perfil: {
+    borderRadius: 999,
+    backgroundColor: "#ffffff",
+
+    borderWidth: 1,
+    borderColor: "#d2d2d2a8",
+
+    height: 90,
+    width: 90,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  areaName: {
+    width: 240,
+    height: 70,
+  },
+
+  TextPerfil: {
+    fontSize: 20,
+    fontWeight: "600",
+  },
+
+  TextID: {
+    marginTop: 2,
+    fontSize: 13,
+    color: "#777",
+  },
+
+  Bio: {
+    marginTop: 2,
+    fontSize: 15,
+    color: "#444",
+  },
+
+  optionAccount: {
+    marginTop: 20,
+    width: 350,
+
+    backgroundColor: "#fff",
+
+    borderRadius: 12,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+
+    shadowOpacity: 0.15,
+    shadowRadius: 4,
+
+    elevation: 5,
+
+    overflow: "hidden",
+  },
+
+  buttonOption: {
+    height: 65,
+
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+
+    paddingHorizontal: 20,
+
+    borderBottomWidth: 1,
+    borderBottomColor: "#ececec",
+  },
+
+  leftOption: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 15,
+  },
+
+  textOption: {
+    fontSize: 17,
+  },
+});
--- src/screens/Perfilscreen.js
+++ src/screens/Perfilscreen.js
@@ -1,9 +1,183 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Perfil() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Perfil</Text>
+    <View style={styles.container}>
+      <View style={styles.AreaPerfil}>
+        <View style={styles.circulo}>
+          <Ionicons name="person" color="#b3b3b3" size={90} />
+        </View>
+
+        <View style={styles.infoPerfil}>
+          <Text style={styles.nome}>Seu Nome</Text>
+
+          <Text style={styles.usuario}>@seunome</Text>
+
+          <View style={styles.localizacao}>
+            <Ionicons name="location-outline" size={16} color="#777" />
+            <Text style={styles.cidade}> São Paulo, Brasil</Text>
+          </View>
+
+          <Text style={styles.bio}>Apaixonado por filmes e séries.</Text>
+        </View>
+      </View>
+      <View style={styles.line}></View>
+      <View style={styles.estatisticas}>
+        <View style={styles.itemEstatistica}>
+          <Text style={styles.numero}>128</Text>
+          <Text style={styles.label}>Assistidos</Text>
+        </View>
+
+        <View style={styles.itemEstatistica}>
+          <Text style={styles.numero}>24</Text>
+          <Text style={styles.label}>Watchlist</Text>
+        </View>
+
+        <View style={styles.itemEstatistica}>
+          <Text style={styles.numero}>12</Text>
+          <Text style={styles.label}>Avaliações</Text>
+        </View>
+      </View>
+      <View style={styles.line_on}></View>
+      <Text style={styles.tittle}>Filmes Recentes</Text>
+    <View style={styles.containerTwo}>
+
+  <View style={styles.AreaCard}>
+    <Image
+      source={{
+        uri: "https://image.tmdb.org/t/p/w500/iiZZdoQBEYBv6id8su7ImL0oCbD.jpg",
+      }}
+      style={styles.poster}
+    />
+  </View>
+
+  <View style={styles.AreaCard}>
+    <Image
+      source={{
+        uri: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg",
+      }}
+      style={styles.poster}
+    />
+  </View>
+
+</View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ffffff",
+    flex: 1,
+  },
+
+  AreaPerfil: {
+    flexDirection: "row",
+    marginTop: 40,
+    marginLeft: 20,
+    alignItems: "center",
+  },
+
+  circulo: {
+    height: 110,
+    width: 110,
+    backgroundColor: "#e0e0e0",
+    borderRadius: 999,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  infoPerfil: {
+    marginLeft: 20,
+    gap: 4,
+  },
+
+  nome: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  usuario: {
+    fontSize: 16,
+    color: "#777",
+  },
+
+  localizacao: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+
+  cidade: {
+    color: "#777",
+    fontSize: 14,
+  },
+
+  bio: {
+    marginTop: 5,
+    fontSize: 15,
+    color: "#444",
+    width: 180,
+  },
+  line: {
+    height: 1,
+    marginTop: 25,
+    backgroundColor: "#bababa6b",
+  },
+  line_on: {
+    marginTop: 25,
+    height: 1,
+    backgroundColor: "#bababa6b",
+  },
+  estatisticas: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 30,
+    backgroundColor: "#f9f9f9",
+    marginHorizontal: 20,
+    paddingVertical: 20,
+    borderRadius: 20,
+  },
+
+  itemEstatistica: {
+    alignItems: "center",
+  },
+
+  numero: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+
+  label: {
+    color: "#777",
+    marginTop: 5,
+  },
+
+  tittle: {
+    fontSize: 20,
+    fontWeight: 500,
+    marginTop: 5,
+    marginLeft: 5,
+    textAlign: "center",
+  },
+  AreaCard: {
+    marginTop: 15,
+    height: 250,
+    width: 150,
+    borderRadius: 10,
+    backgroundColor: "#e7e7e7",
+  },
+  containerTwo: {
+    display: "flex",
+    alignContent: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    gap: 20,
+  },
+  poster: {
+    width: "100%",
+    height: "100%",
+    borderRadius: 10,
+  },
+});
